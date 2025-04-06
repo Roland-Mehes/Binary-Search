1b9d0598a839c1f09e9dcd8f1b99dcbe617dfab5
Roland <mehesroli@gmail.com>
2025-04-06 14:24:32 +0300

    adding new style,and a little UX improvement, basic stuffs

--- index.js
+++ index.js
@@ -1,90 +1,94 @@
-const container = document.getElementById("animation");
+const container = document.getElementById('animation');
 const searchedElementInput = document.querySelector('input[type="number"]');
 const enterBtn = document.querySelector('input[type="number"]~button');
-
 
 let randomNoList = [];
 let animationSteps = [];
 let step = 0;
 
 const renderList = (list, customElem) => {
-    container.innerHTML = "";
-    list.forEach((no, index) => {
-        const box = document.createElement("div");
-        box.className = "list-element";
-        box.style.left = `${index * 30}px`
-        box.style.height = `${(no*2) + 20}px`
-        box.innerText = no;
+  container.innerHTML = '';
+  const width = 30;
 
-        if(customElem && (index === customElem.min || index === customElem.max)){
-            box.style.backgroundColor = "darkblue";
-            box.style.color = "white";
-        }
-        if(customElem && index === customElem.mid){
-            box.style.backgroundColor = "darkred";
-            box.style.color = "white";
-        }
-        container.appendChild(box);
-    })
-}
+  list.forEach((no, index) => {
+    const box = document.createElement('div');
+    box.className = 'list-element';
+    box.style.left = `${index * width}px`;
+    box.style.height = `${no * 2 + 20}px`;
+    box.innerText = no;
 
-const generateList = (length=10, max=100, min=0) => {
-    const resultsArr = [];
-    for (let i=0; i<length; i++) {
-        const newNumber = Math.floor(Math.random() * (max - min)) + min;
-
-        if(resultsArr.includes(newNumber)){
-            length += 1;
-        }
-        else {
-            resultsArr.push(newNumber)
-        }
-
+    if (customElem && (index === customElem.min || index === customElem.max)) {
+      box.style.backgroundColor = 'darkblue';
+      box.style.color = 'white';
     }
-    return resultsArr;
-}
+    if (customElem && index === customElem.mid) {
+      box.style.backgroundColor = 'darkred';
+      box.style.color = 'white';
+    }
+    container.appendChild(box);
+  });
+};
+
+const generateList = (length = 10, max = 100, min = 0) => {
+  const resultsArr = [];
+  while (resultsArr.length < length) {
+    const newNumber = Math.floor(Math.random() * (max - min)) + min;
+
+    if (!resultsArr.includes(newNumber)) {
+      resultsArr.push(newNumber);
+    }
+  }
+  return resultsArr;
+};
 
 const binarySearch = (list, searchElem) => {
-    let min = 0;
-    let max = list.length - 1;
+  let min = 0;
+  let max = list.length - 1;
 
-    while(max >= min) {
-        mid = min + Math.floor((max-min)/2);
-        animationSteps.push({min, max, mid});
-        if(list[mid] === searchElem)return mid;
+  while (max >= min) {
+    let mid = min + Math.floor((max - min) / 2);
+    animationSteps.push({ min, max, mid });
+    if (list[mid] === searchElem) return mid;
 
-        if(list[mid] > searchElem){
-            max = mid - 1;
-            // min-------mid---------max
-            // min------max
-        }
-        
-        else {
-            min = mid + 1
-            // min-------mid---------max
-            //             min-------max
-        }
+    if (list[mid] > searchElem) {
+      max = mid - 1;
+      // min-------mid---------max
+      // min------max
+    } else {
+      min = mid + 1;
+      // min-------mid---------max
+      //             min-------max
     }
-    return -1;
-}
+  }
+  return -1;
+};
 
 const handleNextStep = () => {
-    if(step === animationSteps.length){
-        return;
-    }
-    renderList(randomNoList, animationSteps[step++])
-}
+  if (step >= animationSteps.length) {
+    return;
+  }
+  renderList(randomNoList, animationSteps[step++]);
+};
 
 const main = () => {
-    randomNoList = generateList().sort((a, b) => a - b);
-    renderList(randomNoList);
-}
+  randomNoList = generateList().sort((a, b) => a - b);
+  renderList(randomNoList);
+};
 
-nextStep.addEventListener("click", handleNextStep);
-enterBtn.addEventListener("click", () => {
-    step = 0;
-    animationSteps = [];
-    const searchedElement = searchedElementInput.value;
-    binarySearch(randomNoList, searchedElement);
-})
-window.addEventListener("load", main);
+nextStep.addEventListener('click', handleNextStep);
+
+enterBtn.addEventListener('click', () => {
+  step = 0;
+  animationSteps = [];
+  const searchedElement = Number(searchedElementInput.value);
+  if (isNaN(searchedElement)) {
+    alert('Please use only numbers.');
+    return;
+  }
+
+  const result = binarySearch(randomNoList, searchedElement);
+  if (result === -1) {
+    alert('The searched number is not in the field.');
+  }
+});
+window.addEventListener('load', main);
